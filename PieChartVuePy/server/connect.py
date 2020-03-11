30a7278ce9fb8bb33f95964a0e37d5c1b6dbaf28
kj <kj@ubuntu.ubuntu-domain>
2020-03-11 14:52:50 +0530

    api written

--- PieChartVuePy/server/connect.py
+++ PieChartVuePy/server/connect.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flaskext.mysql import MySQL
+from flask_cors import CORS
 
 app = Flask(__name__)
-mysql = MySQL()
+
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']='root'
 app.config['MYSQL_DATABASE_DB']='testPython'
 app.config['MYSQL_DATABASE_HOST']='localhost'
-mysql.init_app(app)
-conn = mysql.connect()
-cursor = conn.cursor()
-cursor.execute("select * from severity")
-data=cursor.fetchall()
-# console.log(data)
-print(data)
+
+mysql = MySQL(app)
+CORS(app)
+
+@app.route('/api/severity', methods['GET'])
+def get_all_severity():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute("select * from severity")
+    data=cursor.fetchall()
+    # console.log(data)
+    print(data)
